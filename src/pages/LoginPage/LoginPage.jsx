b7fc2cd4586a9d8bf696f4dade2da150a3794e2c
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-27 18:50:30 +0300

    add loadinPage

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,123 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { authOperations, authSelectors } from 'redux/auth';
+import { LoginForm, Input, ErrorMsg } from './LoginPage.styled';
+import Spiner from 'components/Spinner';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import toast from 'react-hot-toast';
+import useSound from 'use-sound';
+
+import boopSfx from 'sounds';
+
+let loginSchema = yup.object().shape({
+  email: yup.string().email(),
+  password: yup
+    .string('Enter your password')
+    .min(6, 'Password should be of minimum 6 characters length')
+    .required('Password is required'),
+});
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/RomaSamkov">
+        My Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function LoginPage() {
+  const [play] = useSound(boopSfx);
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(
+    authSelectors.getIsFetchingCurrentUser
+  );
+
+  const handleSubmit = (value, { resetForm }) => {
+    dispatch(authOperations.logIn(value));
+    toast.success('You Log In in Phonebook!', {
+      duration: 3000,
+      position: 'top-center',
+    });
+    resetForm();
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {isFetchingCurrentUser ? (
+          <Box>{Spiner.customSpinner}</Box>
+        ) : (
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={handleSubmit}
+            validationSchema={loginSchema}
+          >
+            <LoginForm>
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Log in
+              </Typography>
+              <Input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+              ></Input>
+              <ErrorMessage name="email">
+                {msg => <ErrorMsg>{msg}</ErrorMsg>}
+              </ErrorMessage>
+              <Input
+                type="password"
+                name="password"
+                placeholder="Password"
+              ></Input>
+              <ErrorMessage name="password">
+                {msg => <ErrorMsg>{msg}</ErrorMsg>}
+              </ErrorMessage>
+              <Button
+                onClick={play}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Log In
+              </Button>
+              <Link
+                href="https://romasamkov.github.io/goit-react-hw-08-phonebook/register"
+                variant="body2"
+              >
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </LoginForm>
+          </Formik>
+        )}
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </>
+  );
+}
---
+++ src/pages/LoginPage/LoginPage.styled.jsx
@@ -0,0 +1,46 @@
+import { Form, Field } from 'formik';
+import styled from 'styled-components';
+
+export const LoginForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 320px;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
+export const Input = styled(Field)`
+  border-radius: 0.5rem;
+  font-size: 1.25rem;
+  min-height: calc(1em + 1rem + 2px);
+  padding: 0.5rem 1rem;
+  appearance: none;
+  background-clip: padding-box;
+  background-color: #fff;
+  border: 1px solid #b1b7c1;
+  border-radius: 0.375rem;
+  color: rgba(44, 56, 74, 0.95);
+  display: block;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  padding: 0.375rem 0.75rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  width: 90%;
+  margin-top: 10px;
+  &::placeholder {
+  }
+  &:hover,
+  &:focus {
+    box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.75);
+    -webkit-box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.75);
+  }
+`;
+
+export const ErrorMsg = styled('div')`
+  color: red;
+`;
+
+export const AvatarText = styled('h3')``;
