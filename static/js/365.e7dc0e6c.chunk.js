ff0b3b63e915691ccc8cc6059515b402d925445b
VitaliyYerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-27 17:20:17 +0000

    Deploying to gh-pages from @ VitaliyYerin/goit-react-hw-08-phonebook@b7358f699988de27eba65963743dcefece08c247 🚀

---
+++ static/js/365.e7dc0e6c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[365],{5365:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r,c=t(168),i=(0,t(7691).ZP)("div")(r||(r=(0,c.Z)(["\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  align-items: center;\n  height: 70vh;\n  & p {\n  }\n"]))),l=t(184),o=function(){return(0,l.jsxs)(i,{children:[(0,l.jsx)("p",{children:"404 Page Not Found"}),(0,l.jsx)("p",{children:"Try to reload the page"})]})}}}]);
+//# sourceMappingURL=365.e7dc0e6c.chunk.js.map
