1ce46e95d17c0292fc7f4cb19fb5c45bce182535
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-27 22:04:03 +0300

    change ContactListItem.styled.jsx

--- src/components/ContactListItem/ContactListItem.styled.jsx
+++ src/components/ContactListItem/ContactListItem.styled.jsx
@@ -22,32 +22,26 @@ export const Button = styled.button`
   margin: auto;
   cursor: pointer;
   outline: none;
-  padding: 0 10px;
-  width: 80px;
-  height: 30px;
-  background-color: rgb(133, 180, 222);
+  padding: 5px 10px;
+  background-color: #000;
+  color: #fff;
   border: none;
   border-radius: 20px;
   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
     border-color 0.15s ease-in-out;
   cursor: pointer;
-  box-shadow: 0px 0px 4px 4px rgba(58, 132, 222, 0.75);
-  -webkit-box-shadow: 0px 0px 4px 4px rgba(58, 132, 222, 0.75);
-  -moz-box-shadow: 0px 0px 4px 4px rgba(58, 132, 222, 0.75);
   &::first-letter {
     text-transform: uppercase;
   }
   &:hover,
   &:focus {
-    color: #fff;
-    background-color: rgb(100, 70, 192);
-    box-shadow: 0px 0px 7px #fff;
+    box-shadow: 2px 3px 5px #000;
   }
 `;
 
 export const ButtonContainer = styled.div`
   display: flex;
-  column-gap: 20px;
+  column-gap: 10px;
   padding-right: 10px;
   margin-left: 20px;
 `;
