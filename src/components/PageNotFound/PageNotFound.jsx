9a7a8fd7fde4ac8433f5b6a24d935ba72a42ce01
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-27 18:56:29 +0300

    add PageNotFound

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import { Container } from './PageNotFound.styled';
+
+const PageNotFound = () => {
+  return (
+    <Container>
+      <p>404 Page Not Found</p>
+      <p>Try to reload the page</p>
+    </Container>
+  );
+};
+
+export default PageNotFound;
